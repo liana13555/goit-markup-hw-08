70e1f77b41174e7af27edaed88ef08600c02a4d3
Liana Baryshova <barishovaliana@gmail.com>
2021-03-17 01:20:50 +0100

    Кнопка-переключатель для выпадающего меню

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,7 +1,7 @@
   
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
+  // const mobileMenuRef = document.querySelector("[data-menu]");
 
   menuBtnRef.addEventListener("click", () => {
     const expanded =
@@ -10,6 +10,6 @@
     menuBtnRef.classList.toggle("is-open");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
 
-    mobileMenuRef.classList.toggle("is-open");
+    // mobileMenuRef.classList.toggle("is-open");
   });
 })();
